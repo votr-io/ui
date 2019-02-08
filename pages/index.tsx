130491cc483ec6bd3f12d87edd5ed2ae9db3f84c
Erin Noe-Payne <saizul@gmail.com>
2019-02-07 20:30:06 -0700

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const Page = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`
+
+export default () => <Page>Whats up homeslice talk to me</Page>
