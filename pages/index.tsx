3040cb3dab52db1ec193c8eae06db4f3c19887c9
Erin Noe-Payne <saizul@gmail.com>
2019-02-07 21:55:20 -0700

    fun with styles...

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,116 @@
-import styled from 'styled-components'
+import { Flex } from "@rebass/grid";
+import styled from "styled-components";
 
-const Page = styled.div`
+import {
+  elevations,
+  pink,
+  purple_dark,
+  purple_light,
+  text_dark
+} from "../src/styles";
+
+const Page = styled(Flex)`
   position: absolute;
   width: 100%;
   height: 100%;
   top: 0;
   left: 0;
-  display: flex;
   flex: 1;
-  align-items: center;
-  justify-content: center;
-`
+  flex-direction: row;
+`;
 
-export default () => <Page>Whats up homeslice talk to me</Page>
+const Panel = styled(Flex)`
+  flex: 1;
+  padding: 16px;
+`;
+
+const Purple = styled(Flex)`
+  flex: 1;
+  background-image: radial-gradient(
+    ${purple_light.css()},
+    ${purple_dark.css()}
+  );
+  justify-content: center;
+  align-items: center;
+  color: white;
+  font-size: 48px;
+  ${elevations[2].inset}
+  font-family: Arial Black, Arial, Helvetica, sans-serif;
+  font-weight: 800;
+  text-shadow: 2px 2px ${pink.css()};
+`;
+
+const Headline = styled(Flex)`
+  font-family: Arial Black;
+  font-size: 24px;
+  line-height: 36px;
+  color: ${text_dark.css()};
+  text-align: center;
+`;
+
+const Subtitle = styled(Flex)`
+  font-family: Arial;
+  font-size: 18px;
+  line-height: 24px;
+  text-align: center;
+  color: ${text_dark.css()};
+`;
+
+const Button = styled.button`
+  color: ${pink.css()};
+  font-size: 16px;
+  font-weight: bold;
+  padding: 8px 16px;
+  min-width: 64px;
+  border: 2px solid ${pink.css()};
+  border-radius: 2px;
+  background: none;
+  outline: none;
+  cursor: pointer;
+
+  &:hover {
+    background: ${pink.alpha(0.04).css()};
+  }
+`;
+
+const Link = styled.a`
+  font-family: Arial;
+  text-decoration: none;
+  color: ${pink.css()};
+  font-size: 14px;
+  cursor: pointer;
+
+  &:hover {
+    color: ${pink.darken(0.5).css()};
+  }
+`;
+
+const HomePage: React.SFC = () => (
+  <Page>
+    <Panel flexDirection="column">
+      <Flex flex="1" />
+      <Flex
+        flex="1"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Headline>simple ranked-choice voting</Headline>
+        <Subtitle>unleash the power of democracy</Subtitle>
+        <div style={{ height: "48px" }} />
+        <Button>HOLD AN ELECTION ></Button>
+      </Flex>
+      <Flex flex="1" flexDirection="column" justifyContent="flex-end">
+        <Flex flexDirection="row" justifyContent="space-around">
+          <Link>log in</Link>
+          <Link>about us</Link>
+        </Flex>
+      </Flex>
+    </Panel>
+    <Panel>
+      <Purple>votr</Purple>
+    </Panel>
+  </Page>
+);
+
+export default HomePage;
---
+++ src/styles.ts
@@ -0,0 +1,27 @@
+import chroma from "chroma-js";
+import { css } from "styled-components";
+
+export const pink = chroma.hex("#FF55BF");
+export const purple_dark = chroma.hex("#481770");
+export const purple_light = chroma.hex("#A636FF");
+
+export const text_dark = chroma("black").alpha(0.87);
+export const text_light = chroma("black").alpha(0.06);
+
+const makeShadow = (z: number) => {
+  const style = css`
+    box-shadow: 0 ${z}px ${z * 2}px rgba(0, 0, 0, 0.12),
+      0 ${z / 2}px ${z / 2}px rgba(0, 0, 0, 0.24);
+    z-index: ${z};
+  `;
+
+  return Object.assign(style, {
+    inset: css`
+      box-shadow: inset 0 ${z}px ${z * 2}px rgba(0, 0, 0, 0.12),
+        inset 0 ${z / 2}px ${z / 2}px rgba(0, 0, 0, 0.24);
+      z-index: ${-z};
+    `
+  });
+};
+
+export const elevations = [1, 2, 4].map(makeShadow);
