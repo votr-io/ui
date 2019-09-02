02577ba9b739338ed6f8eb572c3d18ab1b11ff0f
Erin Noe-Payne <saizul@gmail.com>
2019-09-02 16:19:32 -0600

    before adding gql macro

--- src/election/ElectionPage.tsx
+++ src/election/ElectionPage.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { RouteComponentProps } from "react-router";
 import { Grid, Typography } from "@material-ui/core";
+import { useQuery } from "@apollo/react-hooks";
+import getElection from "./getElection.gql";
+import { GetElection, GetElectionVariables } from "./generated/GetElection";
 
 // Depending on election status, will be ballot, awaiting results or results
 export const ElectionPage: React.FC<
   RouteComponentProps<{ electionId: string }>
 > = props => {
+  const {} = useQuery<GetElection, GetElectionVariables>(getElection);
+
   return (
     <Grid item>
       <Typography variant="h1">
