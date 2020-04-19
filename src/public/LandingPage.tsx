3c5be89dc0394260533b7d2d803dd6d5aa0552f3
Chris Langager <christopher.langager@gmail.com>
2020-04-19 13:41:03 -0600

    link to create election page

--- src/public/LandingPage.tsx
+++ src/public/LandingPage.tsx
@@ -1,6 +1,10 @@
-import React from "react";
-import { Page } from "../components/Page";
+import React from 'react';
+import { Page } from '../components/Page';
 
 export const LandingPage = () => {
-  return <Page header>Hello world!</Page>;
+  return (
+    <Page header>
+      <a href="/elections/create">Create an Election</a>
+    </Page>
+  );
 };
